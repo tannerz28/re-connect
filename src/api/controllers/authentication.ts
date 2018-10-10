8e6a0792cbc6ed98b33add622d3e436d3c13e3e1
Tanner Goins <tannergz28@gmail.com>
2018-10-09 23:41:50 -0500

    Add method stubs for login and register

--- src/api/controllers/authentication.ts
+++ src/api/controllers/authentication.ts
@@ -1,10 +1,19 @@
-import { Response } from 'express'
-import { controller, httpGet, queryParam, response } from 'inversify-express-utils'
+import { controller, httpPost } from 'inversify-express-utils'
 import { inject } from 'inversify'
 import { Types } from '../constants'
 import { AuthenticationService } from '../services'
 
 @controller('/api/auth')
 export class AuthenticationController {
+  constructor (@inject(Types.AuthenticationService) private authenticationService: AuthenticationService) { }
 
+  @httpPost('/register')
+  async register () {
+
+  }
+
+  @httpPost('/login')
+  async login () {
+
+  }
 }
